cc1e72f654e72e7638dcf09b07b1449bbbe5d5a3
Yash <yashnistane406@gmail.com>
2021-06-28 23:44:35 +0530

    admin can add, delete and update categories

---
+++ src/index.server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
+const path = require('path');
+const cors = require('cors');
+
+
+//routes
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin/auth'); //admin routes
+const categoryRoutes = require('./routes/category'); //categoryroutes
+const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
+const initialDataRoutes = require('./routes/admin/initialData');
+
+
+env.config();
+PORT = process.env.PORT || 5000
+
+//mongo connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.lfm1n.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
+
+  app.use(cors());
+  app.use(express.json());
+  app.use('/public',express.static(path.join((__dirname),'uploads')));
+
+  app.use('/api',authRoutes);
+  app.use('/api',adminRoutes);
+  app.use('/api',categoryRoutes);
+  app.use('/api',productRoutes);
+  app.use('/api',cartRoutes);
+  app.use('/api',initialDataRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
---
+++ src/routes/category.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { requireSignin, adminMiddleware } = require('../common-middleware');
+const router = express.Router();
+const { addCategory, getCategories, updateCategories, deleteCategories } = require('../controller/category');
+
+const multer = require('multer');
+const shortid = require("shortid");
+const path = require("path");
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname), "uploads"));
+    },
+    filename: function (req, file, cb) {
+      cb(null, shortid.generate() + "-" + file.originalname);
+    },
+  });
+
+  const upload = multer({storage});
+
+ router.post('/category/create',requireSignin,adminMiddleware,upload.single('categoryImage'),addCategory);
+//  router.post('/category/create',upload.single('categoryImage'),addCategory);
+ router.get('/category/getCategory',getCategories);
+ router.post('/category/update',upload.array('categoryImage'),updateCategories);
+ router.post('/category/delete',deleteCategories);
+
+ module.exports = router;
