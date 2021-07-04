dca1fbe02b0c815fb060ee4eb0c4dbb9c2e271f4
Yash <yashnistane406@gmail.com>
2021-07-04 17:42:42 +0530

    logged in user can add items to cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { requireSignin, userMiddleware } = require('../common-middleware');
  const router = express.Router();
- const {  addItemToCart} = require('../controller/cart');
+ const {  addItemToCart, getCartItems,} = require('../controller/cart');
 
 
  router.post('/user/cart/addtocart',requireSignin,userMiddleware,addItemToCart);
- 
+ router.post("/user/getCartItems", requireSignin, userMiddleware, getCartItems);
 
  module.exports = router;
