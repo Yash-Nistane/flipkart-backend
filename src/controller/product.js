f931e9ccd3b813e86772212b09a27e4cafd562d8
Yash <yashnistane406@gmail.com>
2021-07-08 14:10:53 +0530

    after placing order, cart is emptied from database

--- src/controller/product.js
+++ src/controller/product.js
@@ -35,56 +35,114 @@ exports.createProduct = (req, res) => {
     });
   };
 
-  exports.getProductBySlug = (req,res) =>{
+  // exports.getProductsBySlug = (req,res) =>{
 
-    const {slug} = req.params;
+  //   const {slug} = req.params;
 
-    Category.findOne({slug:slug})
-    .select('_id')
-    .exec((error,category) => {
-      if(error){ 
-        return res.status(400).json({error});
-      }
+  //   Category.findOne({slug:slug})
+  //   .select('_id')
+  //   .exec((error,category) => {
+  //     if(error){ 
+  //       return res.status(400).json({error});
+  //     }
 
-      if(category){
+  //     if(category){
 
-        Product.find({category:category._id})
-        .exec((error,products) => {
+  //       Product.find({category:category._id})
+  //       .exec((error,products) => {
 
-          if(error){
-            res.status(400).json({error});
-          }
+  //         if(error){
+  //           res.status(400).json({error});
+  //         }
          
-          if(products.length >0){
+          
 
-            res.status(200).json({
-              products,
-              productsByPrice: {
-                under5k: products.filter((product) => product.price <= 5000),
-                under10k: products.filter(
-                  (product) => product.price > 5000 && product.price <= 10000
-                ),
-                under15k: products.filter(
-                  (product) => product.price > 10000 && product.price <= 15000
-                ),
-                under20k: products.filter(
-                  (product) => product.price > 15000 && product.price <= 20000
-                ),
-                under30k: products.filter(
-                  (product) => product.price > 20000 && product.price <= 30000
-                ),
-              }
-            
-            });
-          }
+  //           if(products.length >0){
 
-        })
-      }
+  //             res.status(200).json({
+  //               products,
+  //               productsByPrice: {
+  //                 under5k: products.filter((product) => product.price <= 5000),
+  //                 under10k: products.filter(
+  //                   (product) => product.price > 5000 && product.price <= 10000
+  //                 ),
+  //                 under15k: products.filter(
+  //                   (product) => product.price > 10000 && product.price <= 15000
+  //                 ),
+  //                 under20k: products.filter(
+  //                   (product) => product.price > 15000 && product.price <= 20000
+  //                 ),
+  //                 under30k: products.filter(
+  //                   (product) => product.price > 20000 && product.price <= 30000
+  //                 ),
+  //               }
+              
+  //             });
+  //           }
+          
+          
+
+  //       })
+  //     }
 
       
-    });
+  //   });
     
-  }
+  // }
+
+
+  exports.getProductsBySlug = (req, res) => {
+    const { slug } = req.params;
+    Category.findOne({ slug: slug })
+      .select("_id type")
+      .exec((error, category) => {
+        if (error) {
+          return res.status(400).json({ error });
+        }
+  
+        if (category) {
+          Product.find({ category: category._id }).exec((error, products) => {
+            if (error) {
+              return res.status(400).json({ error });
+            }
+  
+            if (category.type) {
+              if (products.length > 0) {
+                res.status(200).json({
+                  products,
+                  priceRange: {
+                    under5k: 5000,
+                    under10k: 10000,
+                    under15k: 15000,
+                    under20k: 20000,
+                    under30k: 30000,
+                  },
+                  productsByPrice: {
+                    under5k: products.filter((product) => product.price <= 5000),
+                    under10k: products.filter(
+                      (product) => product.price > 5000 && product.price <= 10000
+                    ),
+                    under15k: products.filter(
+                      (product) => product.price > 10000 && product.price <= 15000
+                    ),
+                    under20k: products.filter(
+                      (product) => product.price > 15000 && product.price <= 20000
+                    ),
+                    under30k: products.filter(
+                      (product) => product.price > 20000 && product.price <= 30000
+                    ),
+                  },
+                });
+              }
+            } else {
+              res.status(200).json({ products });
+            }
+          });
+        }
+      });
+  };
+  
+  
 
 
   exports.getProductDetailsById = (req, res) => {
@@ -102,3 +160,8 @@ exports.createProduct = (req, res) => {
       return res.status(400).json({ error: "Params required" });
     }
   };
+
+
+
+  
+  
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct, getProductBySlug, getProductDetailsById } = require('../controller/product');
+const { createProduct, getProductsBySlug, getProductDetailsById } = require('../controller/product');
 const router = express.Router();
 const multer = require('multer');
 
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage});
  router.post('/product/create',requireSignin,adminMiddleware,upload.array('productPicture'),createProduct);
- router.get('/products/:slug',getProductBySlug);
+ router.get('/products/:slug',getProductsBySlug);
 //  router.get('/category/getCategory',getCategories);
  router.get('/product/:productId', getProductDetailsById);
 
