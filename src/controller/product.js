55fea6b161180a20cec427579ea51595f77d074c
Yash <yashnistane406@gmail.com>
2021-07-13 23:04:06 +0530

    can get product by searching

--- src/controller/product.js
+++ src/controller/product.js
@@ -3,7 +3,6 @@ const shortid = require("shortid");
 const slugify = require("slugify");
 const Category = require("../models/category");
 
-
 exports.createProduct = (req, res) => {
   //res.status(200).json( { file: req.files, body: req.body } );
 
@@ -32,7 +31,7 @@ exports.createProduct = (req, res) => {
     if (product) {
       res.status(201).json({ product });
     }
-  }); 
+  });
 };
 
 // exports.getProductsBySlug = (req,res) =>{
@@ -88,7 +87,7 @@ exports.createProduct = (req, res) => {
 exports.getProductsBySlug = (req, res) => {
   const { slug } = req.params;
   Category.findOne({ slug: slug })
-    .select("_id type")
+    .select("_id type slug name")
     .exec((error, category) => {
       if (error) {
         return res.status(400).json({ error });
@@ -136,62 +135,128 @@ exports.getProductsBySlug = (req, res) => {
     });
 };
 
-
-
 exports.getSearchByCatSlug = (req, res) => {
   
-const {name} = req.body;
-const $regex = name;
+  const { name } = req.body;
+  const $regex = name;
 
-   Category.findOne({ slug: { $regex , $options: 'i' } })
-   .select("_id type")
-   .exec((error, category) => {
-     if (error) {
-       return res.status(400).json({ error });
-     }
+  console.log(name);
 
-     if (category) {
-       Product.find({ category: category._id }).exec((error, products) => {
-         if (error) {
-           return res.status(400).json({ error });
-         }
+  Category.findOne({ slug: { $regex, $options: "i" } })
+    .select("_id slug type ")
+    .exec((error, category) => {
+      if (error) {
+        return res.status(400).json({ error });
+      }
 
-         if (category.type){
-           res.status(200).json({ products });
-         }
+       if (category) {
+         Product.find({ category: category._id })
+           .populate({ path: "category", select: "_id name type" })
+           .exec((error, products) => {
+             if (error) {
+               return res.status(400).json({ error });
+             }
 
-       });
-     }
-   });
-  
+             if (category.type) {
+               res.status(200).json({ category });
+             }
+           });
+       }
 
+       
+    });
+};
+
+exports.getProductsByProductName = (req, res) => {
+  const { name } = req.body;
+  const $regex = name;
+
+  console.log(name);
+
+  Product.findOne({ name: { $regex, $options: "i" } })
+    .populate({ path: "category", select: "_id name slug" })
+    .exec((error, product) => {
+
+      Category.findOne({ slug:product.category.slug })
+        .select("_id type slug")
+        .exec((error, category) => {
+          if (error) {
+            return res.status(400).json({ error });
+          }
+
+          if (category) {
+            Product.find({ category: category._id, name:{ $regex, $options: "i"} }).exec((error, products) => {
+              if (error) {
+                return res.status(400).json({ error });
+              }
+
+              if (category.type) {
+                if (products.length > 0) {
+                  res.status(200).json({
+                    products,
+                    priceRange: {
+                      under5k: 5000,
+                      under10k: 10000,
+                      under15k: 15000,
+                      under20k: 20000,
+                      under30k: 30000,
+                    },
+                    productsByPrice: {
+                      under5k: products.filter(
+                        (product) => product.price <= 5000
+                      ),
+                      under10k: products.filter(
+                        (product) =>
+                          product.price > 5000 && product.price <= 10000
+                      ),
+                      under15k: products.filter(
+                        (product) =>
+                          product.price > 10000 && product.price <= 15000
+                      ),
+                      under20k: products.filter(
+                        (product) =>
+                          product.price > 15000 && product.price <= 20000
+                      ),
+                      under30k: products.filter(
+                        (product) =>
+                          product.price > 20000 && product.price <= 30000
+                      ),
+                    },
+                    category,
+                  });
+                }
+              } else {
+                res.status(200).json({ products });
+              }
+            });
+          }
+        });
+    });
 };
 
 exports.getProductDetailsById = (req, res) => {
   const { productId } = req.params;
   if (productId) {
     Product.findOne({ _id: productId })
-    .populate({ path: "category", select: "_id name" })
-    .populate({path:"reviews.userId",select:"_id firstName lastName"  })
-    .exec((error, product) => {
-      if (error) return res.status(400).json({ error });
-      if (product) {
-        //console.log(product);
-        res.status(200).json({ product });
-      }
-    });
+      .populate({ path: "category", select: "_id name" })
+      .populate({ path: "reviews.userId", select: "_id firstName lastName" })
+      .exec((error, product) => {
+        if (error) return res.status(400).json({ error });
+        if (product) {
+          //console.log(product);
+          res.status(200).json({ product });
+        }
+      });
   } else {
     return res.status(400).json({ error: "Params required" });
   }
 };
 
-
 // new update
 exports.deleteProductById = (req, res) => {
   const { productId } = req.body.payload;
   if (productId) {
-    Product.deleteOne({ _id: productId })
-    .exec((error, result) => {
+    Product.deleteOne({ _id: productId }).exec((error, result) => {
       if (error) return res.status(400).json({ error });
       if (result) {
         res.status(202).json({ result });
@@ -202,45 +267,40 @@ exports.deleteProductById = (req, res) => {
   }
 };
 
-
 exports.getProducts = async (req, res) => {
   const products = await Product.find({ createdBy: req.user._id })
-    .select("_id name price quantity slug description productPictures category")  //changed to get category in breed
-    .populate({ path: "category", select: "_id name" })
+    .select("_id name price quantity slug description productPictures category") //changed to get category in breed
+    .populate({ path: "category", select: "_id name " })
     .exec();
 
   res.status(200).json({ products });
 };
 
-
-exports.addReview = async (req,res) => {
-
-  const { productId, review,rating } = req.body;
+exports.addReview = async (req, res) => {
+  const { productId, review, rating } = req.body;
   userId = req.user._id;
 
   Product.findOneAndUpdate(
-    {_id:productId},
+    { _id: productId },
     {
-        $push:{
-          reviews:{
-              userId,
-              review,
-              rating
-          }
-        }
+      $push: {
+        reviews: {
+          userId,
+          review,
+          rating,
+        },
+      },
     },
     {
-      upsert: true
+      upsert: true,
     }
-  )
-  .exec((error, result) => {
-    if(error){
-      return res.status(400).json({error});
+  ).exec((error, result) => {
+    if (error) {
+      return res.status(400).json({ error });
     }
-    
-    if(result){
 
-      return res.status(200).json({result});
+    if (result) {
+      return res.status(200).json({ result });
     }
-  })
-}
+  });
+};
